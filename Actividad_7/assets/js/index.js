40f3850b8b6acee61c30541d3f67d36d5cf613e9
dylan <dpaez@ucol.mx>
2025-05-24 14:52:01 -0600

    Responsity added for Home4

--- Actividad_7/assets/js/index.js
+++ Actividad_7/assets/js/index.js
@@ -3,10 +3,25 @@ document.getElementById('btnAgendarCita').addEventListener('click', function () 
     modal.show();
 });
 
-  // Ejemplo de manejo del formulario (lógica temporal)
 document.getElementById('formAgendarCita').addEventListener('submit', function (e) {
     e.preventDefault();
     alert('Cita agendada correctamente (aún falta guardar en localStorage).');
     bootstrap.Modal.getInstance(document.getElementById('modalAgendarCita')).hide();
     this.reset();
+});
+
+// Botón de la sidebar mostrar/ocultar el sidebar en pantallas pequeñas
+
+document.getElementById('menuToggle').addEventListener('click', () => {
+    const sidebar = document.getElementById('sidebar');
+    const toggleButton = document.getElementById('menuToggle');
+
+    sidebar.classList.toggle('show');
+
+    // Verifica si la sidebar está visible y cambia el color del botón
+    if (sidebar.classList.contains('show')) {
+        toggleButton.classList.add('on-sidebar');
+    } else {
+        toggleButton.classList.remove('on-sidebar');
+    }
 });
